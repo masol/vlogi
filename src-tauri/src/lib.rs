b5243e844d2f52e2db0256508c161e55e6fda5aa
masol <masol.li@gmail.com>
2025-10-07 21:23:51 +0800

    临时提交

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,6 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
-fn greet(name: &str) -> String {
+async fn greet(name: String) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
